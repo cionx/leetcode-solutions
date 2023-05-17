6ffdc452ed4e022ea2c89188f64d541e66c1c2dd
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-17 22:07:56 +0200

    Improve: 2635 in TypeScript.
    Add another solution, which should theoretically be faster. But I’m afraid
    that my use of generics invalidates this improvement.

--- 2635/2635.ts
+++ 2635/2635.ts
@@ -10,8 +10,18 @@ function map<T, U>(arr: T[], fn: (n: T, i :number) => U): U[] {
 // Slighly fancier
 function map<T, U>(arr: T[], fn: (n: T, i :number) => U): U[] {
   const result: U[] = [];
-  for (const kvPair of arr.entries()) {
-    result.push( fn(kvPair[1], kvPair[0]) );
+  for (const [index, value] of arr.entries()) {
+    result.push( fn(value, index) );
+  }
+  return result;
+}
+
+// Create the array already of the correct size for better speed.
+// I don’t know if the use of generics makes this optimization useless.
+function map<T, U>(arr: T[], fn: (n: T, i :number) => U): U[] {
+  const result: U[] = new Array<U>(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = fn(arr[i], i);
   }
   return result;
 }
