8bc8dbf950e7184c8a29b488e4d51dd41172034b
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-18 11:26:10 +0200

    Improve: 2626 in TypeScript
    Make the solution faster, and add a safer variant.

--- 2626/2626.ts
+++ 2626/2626.ts
@@ -1,8 +1,19 @@
 type Fn<T, U> = (accum: U, curr: T) => U
 
 function reduce<T, U>(nums: T[], fn: Fn<T, U>, init: U): U {
-  while (nums.length > 0) {
-    init = fn(init, <T>nums.shift());
+  for (let i = 0; i < nums.length; i++) {
+    init = fn(init, nums[i]);
+  }
+  return init;
+}
+
+
+// the following should be safer, but needs Node.js 17
+function reduce<T, U>(nums: T[], fn: Fn<T, U>, init: U): U {
+  let result = structuredClone(init);
+
+  for (let i = 0; i < nums.length; i++) {
+    result = fn(result, nums[i]);
   }
   return init;
 }
