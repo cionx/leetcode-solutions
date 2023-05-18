b8798dea1de015e0ad92a52e48d16fdaf1290245
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-18 11:27:06 +0200

    Improve: 2629 in TypeScript
    Add a faster variant.

--- 2629/2629.ts
+++ 2629/2629.ts
@@ -1,5 +1,15 @@
 type F<T> = (x: T) => T;
 
+// more elegant
 function compose<T>(functions: F<T>[]): F<T> {
   return (x: T) => functions.reduceRight((y, f) => f(y), x);
+}
+
+// faster
+function compose<T>(functions: F<T>[]): F<T> {
+  return x => {
+    for (let i = functions.length - 1; i >= 0; i--)
+      x = functions[i](x);
+    return x;
+  }
 }
