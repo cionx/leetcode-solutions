35603cb53e86d65e058cd4091d897de5c7bdd1e3
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-18 11:29:09 +0200

    Solution: 2666 in TypeScript.
    Day 8 of the 30 Days of LC JavaScript Challenge.

---
+++ 2666/2666.ts
@@ -0,0 +1,13 @@
+type Fn<T, U> = (...args: T[]) => U;
+
+function once<T, U>(fn: Fn<T, U>): Fn<T, U | undefined> {
+  let internalFn: Fn<T, U | undefined> = fn;
+
+  const newFn = function(...args: T[]) {
+    const value = internalFn(...args);
+    internalFn = () => undefined;
+    return  value;
+  }
+
+  return newFn;
+}
