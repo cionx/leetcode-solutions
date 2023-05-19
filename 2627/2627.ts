ce892a27804eb51a4fe5461c4b80530d0ae07e9d
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-20 00:42:21 +0200

    Solution: 2627 in TypeScript.
    Day 15 of the 30 Days of LC JavaScript Challenge.

---
+++ 2627/2627.ts
@@ -0,0 +1,41 @@
+type Time = number;
+type Call = {
+  time: Time;
+  timer: ReturnType<typeof setTimeout>;
+  rejector: () => void;
+};
+type F<T, U> = (...paras: T[]) => U;
+
+/** The result of a debounded function is a promise, which will resolve after
+    the given waiting period to the function value. If another function call is
+    made before the period is over, then the timer is cancelled and the promise
+    is rejected, befor creating a new promise. **/
+function debounce<T, U>(fn: F<T, U>, timeDiff: Time): F<T, Promise<U | void>> {
+  let prevCall: Call | undefined = undefined;
+
+  return function (...args: T[]): Promise<U | void> {
+    const now: Time = Date.now();
+    if (prevCall != undefined && now - prevCall.time < timeDiff) {
+      clearTimeout(prevCall.timer);
+      prevCall.rejector();
+    }
+
+    let newCall: Call;
+    const preResult = new Promise<U>(function (resolve, reject) {
+      newCall = {
+        time: now,
+        timer: setTimeout(() => resolve(fn(...args)), timeDiff),
+        rejector: reject,
+      };
+    });
+    prevCall = newCall!;
+    return preResult.catch(() => {});
+  };
+}
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
