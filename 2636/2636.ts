7da3358a201a7c07e321e4b5542823ef62490315
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-19 20:36:37 +0200

    Solution: 2636 in TypeScript.
    Day 13 of the 30 Days of LC JavaScript Challenge.

---
+++ 2636/2636.ts
@@ -0,0 +1,33 @@
+type F<T> = () => Promise<T>;
+
+function promisePool<T>(functions: F<T>[], poolSize: number): Promise<void> {
+  return new Promise<void>(function (resolve, _) {
+    if (functions.length == 0) {
+      resolve();
+      return;
+    }
+
+    let i = 0;
+    let resolvedCounter = 0;
+
+    function reportFinished() {
+      resolvedCounter++;
+      if (resolvedCounter == functions.length) {
+        resolve(); // Here we resolve the overall pool promise.
+      }
+      startNext();
+    }
+
+    function startNext() {
+      if (i < functions.length) {
+        functions[i]().finally(reportFinished);
+        i++;
+      }
+    }
+
+    // Initialize the first few promises.
+    for (; poolSize > 0; poolSize--) {
+      startNext();
+    }
+  });
+}
