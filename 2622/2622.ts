1077adaac14ed93427b81b02f3ba1e15ebf94bc4
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-19 23:52:29 +0200

    Solution: 2622 in TypeScript.
    Day 14 of the 30 Days of LC JavaScript Challenge.

---
+++ 2622/2622.ts
@@ -0,0 +1,46 @@
+// The return type of setTimeout varies depending on the environment
+// in which the code is run (browser, deno, Node.js, etc.). We therefore
+// query for this return type.
+type Entry = { value: number; timer: ReturnType<typeof setTimeout> };
+
+class TimeLimitedCache {
+  cache: Map<number, Entry>;
+
+  constructor() {
+    this.cache = new Map();
+  }
+
+  set(key: number, value: number, duration: number): boolean {
+    const prevEntry = this.cache.get(key);
+    const alreadySet = prevEntry != undefined;
+    if (alreadySet) {
+      clearTimeout(prevEntry.timer);
+    }
+
+    const timer = setTimeout(() => this.timeout(key), duration);
+    this.cache.set(key, { value, timer });
+
+    return alreadySet;
+  }
+
+  get(key: number): number {
+    const entry = this.cache.get(key);
+    return entry ? entry.value : -1;
+  }
+
+  count(): number {
+    return this.cache.size;
+  }
+
+  private timeout(key: number) {
+    this.cache.delete(key);
+  }
+}
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
