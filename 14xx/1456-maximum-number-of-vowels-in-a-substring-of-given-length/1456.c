df51c6d3b093fea709659c8117f274a1188f023f
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-25 22:41:53 +0200

    Solution: 1456 in C.

---
+++ 14xx/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456.c
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+
+int maxVowels(char s[], int k);
+bool isVowel(char c);
+
+int maxVowels(char s[], int k)
+{
+	int best = 0;
+	int current = 0;
+
+	for (int j = 0; j < k; j++)
+		current += isVowel(s[j]);
+	best = current;
+
+	for(int i = 0, j = k; s[j] != '\0'; ++i, ++j) {
+		current -= isVowel(s[i]);
+		current += isVowel(s[j]);
+		if (current > best)
+			best = current;
+	}
+
+	return best;
+}
+
+bool isVowel(char c)
+{
+	return c == 'e' || c == 'a' || c == 'o' || c == 'i' || c == 'u';
+}
