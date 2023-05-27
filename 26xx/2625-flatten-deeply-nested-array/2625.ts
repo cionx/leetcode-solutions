451f140115af918a4dcc7c4c92afc585a50afc3e
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-27 22:53:20 +0200

    Solution: 2625 in TypeScript.
    Day 22 of the 30 Days of LC JavaScript Challenge.

---
+++ 26xx/2625-flatten-deeply-nested-array/2625.ts
@@ -0,0 +1,22 @@
+/* The generic type T must not be an Array type. That is, T must not be of the
+ * form U[] for any other type U.
+ */
+type NDArray<T> = (T | NDArray<T>)[];
+
+function flat<T>(arr: NDArray<T>, n: number): NDArray<T> {
+  const result: NDArray<T> = [];
+
+  for (let i = 0; i < arr.length; ++i) {
+    const entry = arr[i];
+    if (!Array.isArray(entry) || n == 0) {
+      result.push(entry);
+      continue;
+    }
+    const recFlattened = flat(entry, n - 1);
+    for (let j = 0; j < recFlattened.length; ++j) {
+      result.push(recFlattened[j]);
+    }
+  }
+
+  return result;
+}
