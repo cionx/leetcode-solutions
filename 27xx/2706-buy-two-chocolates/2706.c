7ee4f21391c2d071c3375537fa230daf629c6e41
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-29 18:19:13 +0200

    Solution: 2706 in C.

---
+++ 27xx/2706-buy-two-chocolates/2706.c
@@ -0,0 +1,26 @@
+#define LARGER_THAN_ANY_PRICE 101
+
+int buyChoco(const int *prices, int pricesSize, int money)
+{
+	int secondCheapest = LARGER_THAN_ANY_PRICE;
+	int cheapest = LARGER_THAN_ANY_PRICE;
+
+	for (int i = 0; i < pricesSize; ++i)
+		if (prices[i] < secondCheapest) {
+			if (prices[i] < cheapest) {
+				secondCheapest = cheapest;
+				cheapest = prices[i];
+			}
+			else {
+				secondCheapest = prices[i];
+			}
+		}
+
+	/* The constraints state that pricesSize >= 2, so both instances of the
+	 * initial value LARGER_THAN_ANY_PRICE will have been replace by actual
+	 * prices. We can therefore compute the total.
+	 */
+	int total = cheapest + secondCheapest;
+
+	return (total <= money) ? (money - total) : money;
+}
