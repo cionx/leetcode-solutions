ebdb0cc27aa3246e2bfc5ad41a360623c848277f
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-29 18:41:14 +0200

    Solution: 2710 in C.

---
+++ 27xx/2710-remove-trailing-zeros-from-a-string/2710.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+char *removeTrailingZeros(char *num)
+{
+	int i = 0;
+
+	/* Search the end of the input string. */
+	while (num[i] != '\0')
+		++i;
+
+	/* Go back to the first non-'0' character. */
+	do
+		--i;
+	while (0 <= i && num[i] == '0');
+
+	/* To get the index shift right: if we end up with i = 0, then the output
+	 * consists of two characters: nums[0] and '\0'. The following code also
+	 * works for the special case i = -1 (but LeetCode excludes this case).
+	 */
+	char *result = malloc((size_t) (i + 2) * sizeof(char));
+	result[i + 1] = '\0';
+
+	for (; i >= 0; --i)
+		result[i] = num[i];
+
+	return result;
+}
