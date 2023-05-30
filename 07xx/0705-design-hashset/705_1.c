bfdbb8ed71b1806f312bc8979728aec0a32addd9
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-30 10:33:43 +0200

    Solution: 705 in C.

---
+++ 07xx/0705-design-hashset/705_1.c
@@ -0,0 +1,42 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX_KEY 1000000
+
+typedef struct {
+	bool contains[MAX_KEY + 1];
+} MyHashSet;
+
+MyHashSet *myHashSetCreate(void);
+void myHashSetAdd(MyHashSet *set, int key);
+void myHashSetRemove(MyHashSet *set, int key);
+bool myHashSetContains(MyHashSet *set, int key);
+void myHashSetFree(MyHashSet *set);
+
+MyHashSet *myHashSetCreate(void)
+{
+	MyHashSet *set = malloc(sizeof(MyHashSet));
+	for (int i = 0; i <= MAX_KEY; ++i)
+		set->contains[i] = false;
+	return set;
+}
+
+void myHashSetAdd(MyHashSet *set, int key)
+{
+	set->contains[key] = true;
+}
+
+void myHashSetRemove(MyHashSet *set, int key)
+{
+	set->contains[key] = false;
+}
+
+bool myHashSetContains(MyHashSet *set, int key)
+{
+	return set->contains[key];
+}
+
+void myHashSetFree(MyHashSet *set)
+{
+	free(set);
+}
---
+++ 07xx/0705-design-hashset/705_2.c
@@ -0,0 +1,49 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX_KEY 1000000
+
+typedef struct {
+	unsigned char bits[(MAX_KEY + 1) / 8 + 1];
+} MyHashSet;
+
+MyHashSet *myHashSetCreate(void);
+void myHashSetAdd(MyHashSet *set, int key);
+void myHashSetRemove(MyHashSet *set, int key);
+bool myHashSetContains(MyHashSet *set, int key);
+void myHashSetFree(MyHashSet *set);
+
+MyHashSet *myHashSetCreate(void)
+{
+	MyHashSet *set = malloc(sizeof(MyHashSet));
+	*set = (MyHashSet) { .bits = {0u} };
+	return set;
+}
+
+void myHashSetAdd(MyHashSet *set, int key)
+{
+	int i = key / 8;
+	key %= 8;
+	unsigned char mask = (unsigned char) (~(~0u << 1) << key);
+	set->bits[i] |= mask;
+}
+
+void myHashSetRemove(MyHashSet *set, int key)
+{
+	int i = key / 8;
+	key = key % 8;
+	unsigned char mask = (unsigned char) ~(~(~0u << 1) << key);
+	set->bits[i] &= mask;
+}
+
+bool myHashSetContains(MyHashSet *set, int key)
+{
+	int i = key / 8;
+	key %= 8;
+	return ((set->bits[i]) >> key) & ~(~0u << 1);
+}
+
+void myHashSetFree(MyHashSet *set)
+{
+	free(set);
+}
