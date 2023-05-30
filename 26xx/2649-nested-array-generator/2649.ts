b81c6c1bd4baa97fbd6c52c040ebe74356f22c8c
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-30 18:38:54 +0200

    Solution: 2649 in TypeScript.
    Day 30 of the 30 Days of LC JavaScript Challenge.

---
+++ 26xx/2649-nested-array-generator/2649.ts
@@ -0,0 +1,13 @@
+type NDArray = (NDArray | number)[];
+
+function* inorderTraversal(arr: NDArray): Generator<number, void, void> {
+  for (let i = 0; i < arr.length; ++i) {
+    const entry = arr[i];
+    if (Array.isArray(entry)) {
+      yield* inorderTraversal(entry);
+    }
+    else {
+      yield entry;
+    }
+  }
+}
