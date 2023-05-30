43ede1edd2a375fd8e2c72dfe42ec7cf85d1aa54
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-30 17:23:20 +0200

    Solution: 2695 in TypeScript.
    Day 28 of the 30 Days of LC JavaScript Challenge.

---
+++ 26xx/2695-array-wrapper/2695.ts
@@ -0,0 +1,20 @@
+class ArrayWrapper {
+  numbers: number[];
+
+  constructor(nums: number[]) {
+    this.numbers = nums;
+  }
+
+  /* valueOf allows class members to be used for arithmetic operations, like
+   * addition, by coercing them. However, writing code in TypeScript that
+   * actually uses this “feature” seems to be a bit of a problem (see
+   * https://github.com/microsoft/TypeScript/issues/2361 for more details).
+   */
+  valueOf(): number {
+    return this.numbers.reduce((acc, summand) => acc + summand, 0);
+  }
+
+  toString(): string {
+    return `[${this.numbers.join(",")}]`;
+  }
+}
