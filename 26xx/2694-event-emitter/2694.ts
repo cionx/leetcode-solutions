d9d3ae0e195fb420ca91ea471e3fd6b2c953da87
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-03 18:37:02 +0200

    Solution: 2694 in TypeScript.
    Day 27 of the 30 Days of LC JavaScript Challenge.

---
+++ 26xx/2694-event-emitter/2694.ts
@@ -0,0 +1,35 @@
+type Callback<T, U> = (...args: T[]) => U;
+type Subscription = { unsubscribe: () => undefined };
+type Event = string;
+
+class EventEmitter<T, U> {
+  private eventRegister: Map<Event, Callback<T, U>[]>;
+
+  constructor() {
+    this.eventRegister = new Map();
+  }
+
+  subscribe(event: Event, callback: Callback<T, U>): Subscription {
+    const cbList = this.eventRegister.get(event);
+    if (cbList == undefined) {
+      this.eventRegister.set(event, [callback]);
+    } else {
+      cbList.push(callback);
+    }
+
+    return {
+      unsubscribe: () => {
+        const list = this.eventRegister.get(event);
+        if (list !== undefined) {
+          this.eventRegister.set(event, list.filter((f) => (f != callback)));
+        }
+        return undefined;
+      },
+    };
+  }
+
+  emit(event: Event, args: T[] = []): U[] {
+    const results = this.eventRegister.get(event)?.map((f) => f(...args));
+    return results || []; /* (undefined || []) is [] */
+  }
+}
