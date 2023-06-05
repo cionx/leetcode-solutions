09a4adef50ca6e4319deee2a7f9a20ad7a4e556b
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-05 09:44:06 +0200

    Solution: 1232 in C.

---
+++ 12xx/1232-check-if-it-is-a-straight-line/1232_1.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+
+/* We check if the given vectors v0, v1, ... are affine dependent: we subtract
+ * v0 from each vector, and then check that each vi - v0 is linear dependent to
+ * v1 - v0. We check that two vectors (a, b) and (c, d) are linearly dependent
+ * by checking that the determinant ad - bc is zero.
+ */
+
+bool checkStraightLine(int **coordinates, int coordinatesSize,
+                       int *coordinatesColSize);
+
+bool checkStraightLine(int **coordinates, int coordinatesSize,
+                       int *coordinatesColSize)
+{
+	const int a = coordinates[1][0] - coordinates[0][0];
+	const int b = coordinates[1][1] - coordinates[0][1];
+
+	for (int i = 2; i < coordinatesSize; ++i) {
+		const int c = coordinates[i][0] - coordinates[0][0];
+		const int d = coordinates[i][1] - coordinates[0][1];
+		if (a * d - b * c != 0)
+			return false;
+	}
+
+	return true;
+}
---
+++ 12xx/1232-check-if-it-is-a-straight-line/1232_2.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+
+/* The line going through the first two points can be described by an equation
+ *   ax + by = c.
+ * The vector (a, b) needs to be orthogonal to the line. It can therefore by
+ * computed by take the difference v1 - v0 and rotating it by 90 degrees.
+ * (Rotation by 90 degrees is given by (x, y) -> (y, -x).) The value c can then
+ * be computed as
+ *   c = ax₀ + by₀.
+ */
+bool checkStraightLine(int **coordinates, int coordinatesSize,
+                       int *coordinatesColSize);
+
+bool checkStraightLine(int **coordinates, int coordinatesSize,
+                       int *coordinatesColSize)
+{
+	const int a = coordinates[1][1] - coordinates[0][1];
+	const int b = -(coordinates[1][0] - coordinates[0][0]);
+	const int c = a * coordinates[0][0] + b * coordinates[0][1];
+
+	for (int i = 2; i < coordinatesSize; ++i)
+		if (a * coordinates[i][0] + b * coordinates[i][1] != c)
+			return false;
+
+	return true;
+}
