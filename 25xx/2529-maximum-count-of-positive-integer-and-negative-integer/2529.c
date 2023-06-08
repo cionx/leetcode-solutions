2d0f466e993e3359ff86b936a456f9f126ca82f6
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-08 11:01:33 +0200

    Solution: 2529 in C.

---
+++ 25xx/2529-maximum-count-of-positive-integer-and-negative-integer/2529.c
@@ -0,0 +1,32 @@
+/* Time: O(log(n))
+ * Space: O(1)
+ */
+
+int maximumCount(int *nums, int numsSize);
+int firstIndexAtLeast(int nums[], int value, int lower, int limit);
+int max(int a, int b);
+
+int maximumCount(int *nums, int numsSize)
+{
+	int i = firstIndexAtLeast(nums, 0, 0, numsSize);
+	int j = firstIndexAtLeast(nums, 1, i, numsSize);
+	return max(i, numsSize - j);
+}
+
+int firstIndexAtLeast(int nums[], int value, int lower, int limit)
+{
+	while (lower < limit) {
+		const int middle = (lower + limit) / 2; /* smaller than limit */
+		if (nums[middle] < value)
+			lower = middle + 1;
+		else
+			limit = middle;
+	};
+
+	return lower;
+}
+
+int max(int a, int b)
+{
+	return (a > b) ? a : b;
+}
