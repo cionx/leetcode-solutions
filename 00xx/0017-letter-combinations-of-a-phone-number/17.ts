851ead5fdc9c8c42f42fa76cefd5423f939a60ca
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-14 00:27:14 +0200

    Solution: 17 in TypeScript.

---
+++ 00xx/0017-letter-combinations-of-a-phone-number/17.ts
@@ -0,0 +1,30 @@
+function letterCombinations(digits: string): string[] {
+  if (digits.length == 0) {
+    return [];
+  }
+
+  const letters = [
+    [],
+    [],
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"],
+    ["j", "k", "l"],
+    ["m", "n", "o"],
+    ["p", "q", "r", "s"],
+    ["t", "u", "v"],
+    ["w", "x", "y", "z"],
+  ];
+
+  function crossCombine(arr1: string[], arr2: string[]): string[] {
+    return arr1
+      .map((first) => arr2.map((second) => first + second))
+      .flat();
+  }
+
+  return Array
+    .from(digits)
+    .map((char) => parseInt(char)) /* JS is broken like that */
+    .map((digit) => letters[digit])
+    .reduce(crossCombine); /* This line would crash if digits were empty. */
+}
