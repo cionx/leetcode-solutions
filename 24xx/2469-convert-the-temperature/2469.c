fccb092402047f064520b7a55a11a6365a63be8a
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-13 23:02:19 +0200

    Solution: 2469 in C.

---
+++ 24xx/2469-convert-the-temperature/2469.c
@@ -0,0 +1,14 @@
+/* Time: O(1)
+ * Space: O(1)
+ */
+
+#include <stdlib.h>
+
+double *convertTemperature(double celsius, int *returnSize)
+{
+	double *result = malloc(2 * sizeof(double));
+	result[0] = celsius + 273.15;    /* Kelvin */
+	result[1] = celsius * 1.8 + 32.; /* Fahrenheit */
+	*returnSize = 2;
+	return result;
+}
