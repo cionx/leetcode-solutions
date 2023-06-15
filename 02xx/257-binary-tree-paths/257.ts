e1baa1e4c91e583777a825f06b51a60b86906b8b
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-15 16:44:27 +0200

    Solution: 257 in TypeScript.

---
+++ 02xx/257-binary-tree-paths/257.ts
@@ -0,0 +1,28 @@
+/* Recursive solution, bottom to top
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+/* Do not copy the above class definition to LeetCode. */
+
+function binaryTreePaths(root: TreeNode | null): string[] {
+  if (root === null) {
+    return [];
+  }
+  if (root.left === null && root.right === null) {
+    return [`${root.val}`];
+  }
+  const prefix = `${root.val}->`;
+  return binaryTreePaths(root.left)
+    .concat(binaryTreePaths(root.right))
+    .map((path) => prefix + path);
+}
