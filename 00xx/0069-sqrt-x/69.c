fa751c0830707b0d0c2bad425dbe06c5cefd9cd5
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-19 20:31:24 +0200

    Solution: 69 in C.

---
+++ 00xx/0069-sqrt-x/69.c
@@ -0,0 +1,22 @@
+/* Time: O(log(n))
+ * Space: O(1)
+ *
+ * where n is the input non-negative integer. */
+
+int mySqrt(int n)
+{
+	int lower = 0, upper = n;
+
+	while (lower < upper) {
+		const int middle = lower + (upper - lower - 1) / 2 + 1; /* rounded up */
+		const long long sqr = (long long) middle * middle;
+		if (sqr > n)
+			upper = middle - 1;
+		else if (sqr < n)
+			lower = middle;
+		else
+			return middle;
+	}
+
+	return lower;
+}
