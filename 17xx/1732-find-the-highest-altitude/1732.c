dcedd4679311259aa8dddb2465a2036615d69c48
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-19 02:44:08 +0200

    Solution: 1732 in C.

---
+++ 17xx/1732-find-the-highest-altitude/1732.c
@@ -0,0 +1,19 @@
+/* Time: O(n)
+ * Space: O(1)
+ *
+ * where n is the lenght of the input list.
+ */
+
+/* The numbers are small enough that no overflow will occur. */
+
+int largestAltitude(int *gain, int gainSize)
+{
+	int max = 0, current = 0;
+	for (int i = 0; i < gainSize; ++i) {
+		current += gain[i];
+		if (current > max)
+			max = current;
+	}
+
+	return max;
+}
