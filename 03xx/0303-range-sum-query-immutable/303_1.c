73f30bf5a3bebe99c648c6c079fea1347b49bc03
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-19 08:21:20 +0200

    Solution: 303 in C.

---
+++ 03xx/0303-range-sum-query-immutable/303_1.c
@@ -0,0 +1,34 @@
+/* numArrayCreate:
+ * Time: O(1);
+ * Space: (1);
+ *
+ * numArraySumRange:
+ * Time: O(right - left);
+ * Space: O(1);
+ */
+
+#include <stdlib.h>
+
+typedef struct {
+	int *nums;
+} NumArray;
+
+NumArray *numArrayCreate(int *nums, int numsSize)
+{
+	NumArray *arr = malloc(sizeof(NumArray));
+	arr->nums = nums;
+	return arr;
+}
+
+int numArraySumRange(NumArray *arr, int left, int right)
+{
+	int sum = 0;
+	for (int i = left; i <= right; ++i)
+		sum += arr->nums[i];
+	return sum;
+}
+
+void numArrayFree(NumArray *arr)
+{
+	free(arr);
+}
---
+++ 03xx/0303-range-sum-query-immutable/303_2.c
@@ -0,0 +1,41 @@
+/* numArrayCreate:
+ * Time: O(n);
+ * Space: O(n);
+ *
+ * numArraySumRange:
+ * Time: O(1);
+ * Space: O(1);
+ *
+ * where n is the lengt of the input array
+ */
+
+#include <stdlib.h>
+
+typedef struct {
+	int *sums;
+} NumArray;
+
+NumArray *numArrayCreate(int *nums, int numsSize)
+{
+	NumArray *arr = malloc(sizeof(NumArray));
+	arr->sums = malloc((size_t) numsSize * sizeof(int));
+
+	int sum = 0;
+	for (int i = 0; i < numsSize; ++i) {
+		sum += nums[i];
+		arr->sums[i] = sum;
+	}
+
+	return arr;
+}
+
+int numArraySumRange(NumArray *arr, int left, int right)
+{
+	return arr->sums[right] - ((left > 0) ? arr->sums[left - 1] : 0);
+}
+
+void numArrayFree(NumArray *arr)
+{
+	free(arr->sums);
+	free(arr);
+}
