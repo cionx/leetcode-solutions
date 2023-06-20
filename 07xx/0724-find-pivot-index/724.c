90461c90ab967eef90c5e2745dd9c72278e72de8
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-20 22:22:27 +0200

    Solution: 724, 1991 in C.
    The problems are the same.

---
+++ 07xx/0724-find-pivot-index/724.c
@@ -0,0 +1,27 @@
+/* 724. Find Pivot Index */
+
+/* Complexity.
+ * Time: O(n)
+ * Space: O(1)
+ * where n is the length of the input list.
+ */
+
+/* Note.
+ * This problem is the same as 1991 (Find the Middle Index in Array). */
+
+int pivotIndex(int *nums, int numsSize)
+{
+	long leftSum = 0; /* Holds values between -1e7 and 1e7. */
+	long rightSum = 0;
+	for (int i = 0; i < numsSize; ++i)
+		rightSum += nums[i];
+
+	for (int i = 0; i < numsSize; ++i) {
+		rightSum -= nums[i];
+		if (leftSum == rightSum)
+			return i;
+		leftSum += nums[i];
+	}
+
+	return -1;
+}
