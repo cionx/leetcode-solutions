c5827cbad8bbf23426fa579b4970bb1de037dbb2
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-28 21:09:19 +0200

    Improve: 26.

--- 00xx/0026-remove-duplicates-from-sorted-array/26.c
+++ 00xx/0026-remove-duplicates-from-sorted-array/26.c
@@ -6,11 +6,11 @@
  * where n is the length of the input array. */
 
 int removeDuplicates(int* nums, int numsSize){
-	int k = 0;
+	int to = 0;
 
-	for (int i = 1; i < numsSize; ++i)
-		if (nums[k] != nums[i])
-			nums[++k] = nums[i];
+	for (int from = 1; from < numsSize; ++from)
+		if (nums[to] != nums[from])
+			nums[++to] = nums[from];
 
-	return k + 1;
+	return to + 1;
 }
