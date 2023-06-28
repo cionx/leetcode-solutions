8ad89d2d9a981e19a1226b9fed319f512c264258
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-28 20:03:55 +0200

    Improve: 27.

--- 00xx/0027-remove-element/27.c
+++ 00xx/0027-remove-element/27.c
@@ -1,16 +1,16 @@
 /* 27. Remove Elements */
 
-/* Complexities
+/* Complexities.
  * Time: O(n)
  * Space: O(1)
  * where n is the length of the input array. */
 
 int removeElement(int* nums, int numsSize, int val){
-	int k = 0;
+	int to = 0;
 
-	for (int i = 0; i < numsSize; ++i)
-		if (nums[i] != val)
-			nums[k++] = nums[i];
+	for (int from = 0; from < numsSize; ++from)
+		if (nums[from] != val)
+			nums[to++] = nums[from];
 
-	return k;
+	return to;
 }
