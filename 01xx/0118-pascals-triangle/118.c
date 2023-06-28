ed0280308d457395bf113845eaef857561e554f4
Jendrik Stelzner <jendrikstelzner@web.de>
2023-06-28 03:22:55 +0200

    Improve: 118.

--- 01xx/0118-pascals-triangle/118.c
+++ 01xx/0118-pascals-triangle/118.c
@@ -3,23 +3,22 @@
 /* Complexities.
  * Time: O(n^2)
  * Space: O(1)
- * where n is the input array. */
+ * where n is the length of the input array. */
 
 #include <stdlib.h>
 
-int **generate(int n, int *returnSize, int **returnColumnSizes)
+int **generate(int n, int *returnSize, int **returnColSizes)
 {
+	*returnColSizes = malloc((size_t) n * sizeof(int));
 	*returnSize = n;
-	*returnColumnSizes = malloc((size_t) n * sizeof(int));
-	for (int i = 0; i < n; ++i)
-		(*returnColumnSizes)[i] = i + 1;
 
 	int **result = malloc((size_t) n * sizeof(int *));
-	for (int row = 1; row <= n; ++row) {
-		const int rowIndex = row - 1;
-		result[rowIndex] = malloc((size_t) row * sizeof(int));
-		result[rowIndex][0] = result[rowIndex][row - 1] = 1;
-		for (int i = 1; i < row - 1; ++i)
+	for (int rowSize = 1; rowSize <= n; ++rowSize) {
+		const int rowIndex = rowSize - 1;
+		(*returnColSizes)[rowIndex] = rowSize;
+		result[rowIndex] = malloc((size_t) rowSize * sizeof(int));
+		result[rowIndex][0] = result[rowIndex][rowSize - 1] = 1;
+		for (int i = 1; i < rowSize - 1; ++i)
 			result[rowIndex][i] =
 				result[rowIndex - 1][i] + result[rowIndex - 1][i - 1];
 	}
