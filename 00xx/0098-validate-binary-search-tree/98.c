2ac470b372b7d5e118f9326e8526250b394d292f
Jendrik Stelzner <jendrikstelzner@web.de>
2023-07-06 14:45:52 +0200

    Solution: 98 in C.

---
+++ 00xx/0098-validate-binary-search-tree/98.c
@@ -0,0 +1,46 @@
+/* 98. Validate Binary Search Tree */
+
+/* Recursive solution. */
+
+/* Complexities.
+ * Time: O(n)
+ * Space O(h)
+ * where n is the number of nodes in the tree and h is the height of the tree.
+ */
+
+#include <limits.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/* Do not copy the below struct definition to LeetCode. */
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+};
+
+bool isValidBST(struct TreeNode *root);
+bool isBSTBetween(struct TreeNode *root, int lower, int upper);
+
+/* isValidBST(root): checks if the binary tree `root` is a BST. */
+bool isValidBST(struct TreeNode *root)
+{
+	return isBSTBetween(root, INT_MIN, INT_MAX);
+}
+
+/* isBSTBetween(root, lower, upper): Checks whether `root` is a BST whose node
+ * values all lie between `lower` and `upper` (inclusive). */
+bool isBSTBetween(struct TreeNode *root, int lower, int upper)
+{
+	return root == NULL ||
+	       /* Check root value. */
+	       lower <= root->val && root->val <= upper &&
+	           /* Check left subtree. */
+	           (root->val == INT_MIN
+	                ? (root->left == NULL)
+	                : isBSTBetween(root->left, lower, root->val - 1)) &&
+	           /* Check right subtree. */
+	           (root->val == INT_MAX
+	                ? (root->right == NULL)
+	                : isBSTBetween(root->right, root->val + 1, upper));
+}
