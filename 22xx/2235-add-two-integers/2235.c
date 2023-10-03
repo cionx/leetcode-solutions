6b5ecb90742beccad40717dafc971628779bd9be
Jendrik Stelzner <jendrikstelzner@web.de>
2023-10-03 14:59:17 +0200

    Solution: 2235 in C.

---
+++ 22xx/2235-add-two-integers/2235.c
@@ -0,0 +1,13 @@
+/* 2235. Add Two Integers */
+
+/* Complexities.
+ * Time: Θ(1)
+ * Space: Θ(1) */
+
+/* Idea.
+ * Use the built-in addition. */
+
+int sum(int num1, int num2)
+{
+	return num1 + num2;
+}
