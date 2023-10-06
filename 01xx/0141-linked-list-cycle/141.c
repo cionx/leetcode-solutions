54d3bf2ce0ef3a065f976984e92111b90529bd89
Jendrik Stelzner <jendrikstelzner@web.de>
2023-10-06 20:08:16 +0200

    Improve: 141.

--- 01xx/0141-linked-list-cycle/141.c
+++ 01xx/0141-linked-list-cycle/141.c
@@ -2,27 +2,29 @@
 
 /* Complexities.
  * Time: O(n)
- * Space: O(1)
+ * Space: Θ(1)
  * where n is the length of the input list. */
 
 /* Idea.
+ * We use Floyd’s cycle finding algorithm:
+ *
  * We go through the list with two pointers of different speed. The slow pointer
  * moves by one node at a time whereas the fast pointer moves by two nodes at a
  * time.
  * - If the list has no cycle then the fast pointer will eventuall arrive at
- *   NULL, in which case we return `true`.
+ *   NULL, in which case we return `false`.
  * - If the list has a cycle, then the slow pointer enters this cycle at some
- *   point in time. The fast pointer is then also trapped in the cycle. The fast
- *   pointer will then overtake the slow pointer at some future point in time.
- *   As the speed difference between the two pointers is exactly one node, this
- *   will entail that the two pointers meet at some node. We can then return
- *   `true`.
- * (This is Floyd’s cycle finding algorithm.) */
+ *   point in time. The fast pointer is then also already trapped in the cycle.
+ *   The fast pointer will then overtake the slow pointer at some future point
+ *   in time. As the speed difference between the two pointers is exactly one
+ *   node, this will entail that the two pointers meet at some node. We can then
+ *   return `true`.
+ */
 
 #include <stdbool.h>
 #include <stdlib.h>
 
-/* Do not copy the below struct definition to LeetCode. */
+/* Do not copy this struct definition to LeetCode. */
 struct ListNode {
 	int val;
 	struct ListNode *next;
